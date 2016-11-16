646964af2c93ac8f2aca7bcada83f2f9bc36023b
Amanda Cardona <amandacardona@uagateway.org>
2016-11-16 17:16:16 +0000

    worked on unit 4 lab

--- Unit 4/ACardona_rps.py
+++ Unit 4/ACardona_rps.py
@@ -5,7 +5,7 @@ import random
 #   purpose: present player with options, use input() to get player move
 #   returns: the player's move as either 'r', 'p', or 's'
 def get_p1_move():
-    move = input("r, p, s")
+    move = input("r, p, s:")
     return move
      
 
@@ -17,18 +17,21 @@ def get_p1_move():
 def get_comp_move():
     randy = random.randint(1,3)
     if randy == 1:
-        return 'rock'
+        return ("rock")
     elif randy == 2:
-        return 'paper'
+        return ("paper")
     elif randy == 3:
-        return 'scissors'
+        return ("scissors")
+    return randy
+    
 
 #function name: get_rounds
 #   arguments: none
 #   purpose: allows the user to choose a number of rounds from 1 to 9.
 #   returns: the user-chosen number of rounds
 def get_rounds():
-    return 1
+    rounds = input(int("How many rounds?"))
+    return rounds
 
 #function name: get_round_winner
 #   arguments: player move, computer move
@@ -39,6 +42,23 @@ def get_rounds():
 #               "comp" if computer won
 #               "tie" if it's a tie
 def get_round_winner(p1move, cmove):
+    if p1move == cmove:
+        return("Tie!")
+    elif p1move == 'scissors' and cmove == 'paper':
+        return("You win!")
+    elif p1move == 'paper' and cmove == 'rock':
+        return("You win!")
+    elif p1move == 'rock' and cmove == 'scissors':
+        return("You win!")
+    elif p1move == 'rock' and cmove == 'paper':
+        return("You lose!")
+    elif p1move == "paper" and cmove == 'scissors':
+        return("You lose!")
+    elif p1move == 'scissors' and cmove == 'rock':
+        return("You lose")
+    else:
+        print("Error")
+    
     return 1
 #function name: get_full_move
 #   arguments: a single letter move 'r','p', or 's'
@@ -55,7 +75,7 @@ def get_full_move(shortmove):
 #   purpose: prints the scoreboard
 #   returns: none
 def print_score(pscore, cscore, ties):
-    return 1
+    print(get_rounds())
 
 #function name: rps
 #   arguments: none
@@ -72,5 +92,8 @@ def rps():
 #   returns: none
 def test():
     return 1
-
+print(get_rounds())
+print(get_p1_move()) 
+print(get_comp_move())
+print(get_round_winner())
 rps()
