c94bd4c0ad8185df08c02b3d1d98df6a494e9a50
Amanda Cardona <amandacardona@uagateway.org>
2016-12-13 17:15:45 +0000

    worked on craps lab

--- Unit 4/ACardona_craps.py
+++ Unit 4/ACardona_craps.py
@@ -3,20 +3,23 @@ import random
 def bank_account():
     bank_account == 100
     
-def get_bet():
-    bet = int(input("Enter a whole number for your bet:"))
-    if bet < 0:
-        return("This is an invalid amount. Your bet must be a positive integer!")
-    if bet > 100:
-        return("This is over your available amount.")
-    return bet
+def get_bet(bank_account):
+    while True:
+        bet = int(input("Enter a whole number for your bet:"))
+        if bet < 0:
+            return("This is an invalid amount. Your bet must be a positive integer!")
+        if bet > 100:
+            return("This is over your available amount.")
+        else:
+            return bet
     
     
-def first_roll_result():
+def roll2dice():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     dice_sum = dice1 + dice2
     print("Rolled 2 dice: {} {}".format(dice1,dice2))
+    print("Total: {}".format(dice1 + dice2))
     return dice_sum
  
 
@@ -29,34 +32,49 @@ def first_roll_result():
 #if roll  is 2,3,12: return"lose"
 
     
-def first_roll(dice_sum,point_roll):
+def first_roll_result(dice_sum):
     if dice_sum == 7 or dice_sum == 11:
         return("You win!")
     if dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
         return("You lose")
     else:
-        dice_sum == point_roll
-        
-        
-
-def phase_three_bet(bank_account):
-    p3_bet = input("Do you want to keep playing? Yes or No?")
-    if p3_bet == "Yes":
-        return("You have {} in your bank account.".format(get_bet))
-    if p3_bet == "No":
-        return("Thank you for playing.")
-        bank_account = bank_account
-        
-def phase_three(dice_sum, point_roll):
-    point_roll = first_roll_result()
-    if dice_sum == 7:
-        return("You lose")
-    if dice_sum == point_roll:
-        return("You win!")
+        return dice_sum
     
-def phase_winner(point_roll, bank_account):
-    if dice_sum == point_roll or dice_sum == 7:
-        return bet
+def second_roll_result(dice_sum,point_roll):
+    if dice_sum == 7 or dice_sum == point_roll:
+        return("You won!")
+    if dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
+        return("You lose!")
+    else:
+        return("TRY AGAIN")
+
+        
+def craps():
+    bank_account = 100
+    get_bet(bank_account)
+    dice = roll2dice
+    first_result = first_roll_result(dice)
+    if first_result == "You won!":
+        print("You won!")
+    elif first_result == "You lose!":
+        print("You lose!")
+    else:
+        print("point roll")
+        dice = roll2dice()
+        point_roll_result = second_roll_result(dice,first_result)
+        
+craps()
+        
+#def phase_three(dice_sum, point_roll):
+#    point_roll = first_roll_result()
+#    if dice_sum == 7:
+#        return("You lose")
+#    if dice_sum == point_roll:
+#        return("You win!")
+#    
+#def phase_winner(point_roll, bank_account):
+#    if dice_sum == point_roll or dice_sum == 7:
+#        return 
          
     # return twice the amount of betted money to the winner
 # if computer or dealer wins, give the money to the dealer
