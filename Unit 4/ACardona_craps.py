020c9c0575a36c68d4b453750da683e129910b0b
Amanda Cardona <amandacardona@uagateway.org>
2017-01-05 17:14:44 +0000

    worked on underover7

--- Unit 4/ACardona_craps.py
+++ Unit 4/ACardona_craps.py
@@ -1,7 +1,7 @@
 import random
 
     
-def get_bet(bank_account):
+def bet(bank_account, bet):
     while True:
         print("You have ${} in your bank account".format(bank_account))
         bet = int(input("Enter a whole number for your bet: $"))
@@ -31,63 +31,71 @@ def roll2dice():
 #if roll  is 2,3,12: return"lose"
 
     
-def first_roll_result(dice_sum):
+def first_roll_result(dice_sum, bank_account):
     if dice_sum == 7 or dice_sum == 11:
+        bank_account = bank_account + bet
         print("You win!")
-        return "You win!"
-    elif dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
+        print("You have ${} in your bank account".format(bank_account)) 
+    if dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
+        bank_account = bank_account - bet
         print("You lose")
-        return "You lose!"
+        return("You lose!")       
     else:
         return dice_sum
-        
-def point_roll_result(point_roll):
-    if dice_sum != 7 or dice_sum != 11:
-        dice_sum = point_roll
+    print("You have ${} in your bank account".format(bank_account)) 
+ # lines 34-42 is made to determine whether the number is a losing number or a winning number which is point roll.
+ # point roll is the number that was rolled in the first round but it can't equal 7. Point roll is any winning number for the next round.
+ 
+def point_roll_result(point_roll, dice_sum):
+    if dice_sum == 7:
+        return("You lose!")
+    elif dice_sum != 7:
+        return("You win!")
+    
+    
     
 def second_roll_result(dice_sum,point_roll):
+    if dice_sum == 7:
+        print("You lose")
     if dice_sum == point_roll:
-        print("You win!")
-        return(craps())
-    elif dice_sum == 7 or dice_sum != point_roll:
-        print("You lose!")
-        return(craps())
+        print("You win")
     else:
         while (dice_sum != 7 and dice_sum != point_roll):
             dice_sum = roll2dice()
-            return(second_roll_result)                                     
+            if dice_sum == 7:
+                print("You lose!")
+                return "You lose!"
+            elif dice_sum == point_roll:
+                print("You won!")
+                return "You won"
+   
         
 
 
         
 def craps():
     bank_account = 100
-    get_bet(bank_account)
-    dice = roll2dice
-    first_result = first_roll_result(dice)
+    bet(bank_account, bet)
+    dice = roll2dice()
+    first_result = first_roll_result(dice, bank_account)
     if first_result == "You win!":
+        bank_account = bank_account + bet
         print("You win!")
-    elif first_result == "You lose!":
-        print("You lose!")
+        print("You have ${} in your bank account".format(bank_account)) 
     else:
         print("point roll")
-        diceroll = roll2dice()
-        point_roll_result = second_roll_result(dice,first_result)
-        if point_roll_result == "You win!":
-            print("You win!")
-        elif point_roll_result == "You lose!":
-            print("You lose!")
-            
-        else:
-            print("point_roll")
-            diceroll = roll2dice()
-            point_roll_result = second_roll_result(dice,first_result)
-            print ("_________________________________")
-            while bank_account > 0:
-                if bank_account <= 0:
-                    return "You don't have enough in your bank account"
-                elif bank_account > 0:
-                    return craps()
+    diceroll = roll2dice()
+    point_roll_result = second_roll_result(dice,first_result)
+    if point_roll_result == "You win!":
+        print("You win!")
+    if point_roll_result == "You lose!":
+        bank_account = bank_account - bet
+        print("You lose!")
+        print("You have ${} in your bank account".format(bank_account)) 
+    diceroll = roll2dice()
+    point_roll_result = second_roll_result(dice,first_result)
+    print ("_________________________________")
+           
         
 craps()
         
