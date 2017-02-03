47721b534899f2e8b47050bd725bbbaf460f5e11
Amanda Cardona <amandacardona@uagateway.org>
2017-02-03 17:14:52 +0000

    tried to take out first and last letter to scramble word

--- Unit 5/acardona_scramble.py
+++ Unit 5/acardona_scramble.py
@@ -1,6 +1,14 @@
 import random
-
+wlist = []
+word = "myself"
+print (word)
 #making a list
 #mocking users input = index aka list
-#scamble users input
+#scramble users input
 #print users input
+def scramble_word(word):
+    list(word)
+    same_letter == [0:-1]
+    
+    
+def scramble_phrase():
