929ff935e7f884d092f4a914682d273d65794345
Amanda Cardona <amandacardona@uagateway.org>
2017-02-15 17:15:55 +0000

    made keys and user inputs value

---
+++ New Folder/dictionary.py
@@ -0,0 +1,12 @@
+from pprint import pprint
+
+x = input("What would you like the key to be called?")
+v = input("What would you like the value of the key to be?")
+
+d = {
+    
+}
+d[x] = v
+pprint(d)
+
+ 
--- Unit 5/acardona_scramble.py
+++ Unit 5/acardona_scramble.py
@@ -6,14 +6,14 @@ print (word)
 #mocking users input = index aka list
 #scramble users input
 #print users input
-def scramble_word(word):
+def scramble_word(word):        
     if len(word) >= 4:
         split = list(word)
         first = split[0]
         last = split[-1]
         firstlast_l = split[0:-1]
         random.shuffle(firstlast_l)
-        firstlast_l.append(0, first)
+        firstlast_l.insert(0, first)
         firstlast_l.append(last)
         final = ''.join(firstlast_l)
         print(final)
