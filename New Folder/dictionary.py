4b1ce7bbba59e0b508f0fc40254c8519e119aa9a
Amanda Cardona <amandacardona@uagateway.org>
2017-02-16 17:16:29 +0000

    worked on stuff

--- New Folder/dictionary.py
+++ New Folder/dictionary.py
@@ -1,12 +1,18 @@
 from pprint import pprint
-
-x = input("What would you like the key to be called?")
-v = input("What would you like the value of the key to be?")
-
-d = {
-    
-}
-d[x] = v
-pprint(d)
-
- 
+def dictionary():
+    u = input("What would you like to do? Add? Delete? Or exit?")
+    d = {}
+while True:
+    if u == 'add':
+        x = input("What would you like the key to be called?")
+        y = input("What would you like the value of the key to be?")
+        pprint (d)
+    if u == 'delete':
+        k = input("What key would you like to delete?")
+    if k in (d):
+        del d[k]
+        pprint (d)
+    else:
+        print("Exit")
+        break
+dictionary() 
