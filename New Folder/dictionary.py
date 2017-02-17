0497f7105f6f41be7905710d0b5a75aaaf351185
Amanda Cardona <amandacardona@uagateway.org>
2017-02-17 17:15:32 +0000

    finished dictionary project by making modify value and editing functions

--- New Folder/dictionary.py
+++ New Folder/dictionary.py
@@ -1,18 +1,29 @@
 from pprint import pprint
 def dictionary():
-    u = input("What would you like to do? Add? Delete? Or exit?")
     d = {}
-while True:
-    if u == 'add':
-        x = input("What would you like the key to be called?")
-        y = input("What would you like the value of the key to be?")
-        pprint (d)
-    if u == 'delete':
-        k = input("What key would you like to delete?")
-    if k in (d):
-        del d[k]
-        pprint (d)
-    else:
-        print("Exit")
-        break
+    while True:
+        u = input("What would you like to do? Add? Delete? Modify? Or exit?")
+        if u == 'add':
+            x = input("What would you like the key to be called?")
+            if x in d:
+                print("This key already exists.")
+            y = input("What would you like the value of the key to be?")
+            d[x] = y
+            pprint (d)
+        if u == 'delete':
+            k = input("What key would you like to delete?")
+            if k in (d): 
+                del d[k]
+            else:
+                print("Sorry, key was not found.")
+            pprint (d)
+        if u == "modify":
+            x = input("What would you like to modify?")
+            if x in (d):
+               m = input("What would you like to change the value to?")
+               d[x] = m
+               pprint(d)
+        if u == 'Exit':
+            print("Exit")
+            break
 dictionary() 
